816f94ce250fcdc6eada7f821af1f7a4a88420bf
jose-abel <guest1@guest1s-MacBook-Pro.local>
2022-09-15 12:25:22 -0400

    Add the navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+
+const Shop = () => {
+  return (<h1>I am the shop page</h1>)
+}
 
 const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}> 
+        <Route index element={<Home />}/>
+        <Route path="shop" element={<Shop />}/>
+      </Route>
     </Routes>
   );
 }
