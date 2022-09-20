03bdb6b6c2c15ac970dd2ccaa65add118bfb7814
jose-abel <guest1@guest1s-MacBook-Pro.local>
2022-09-20 06:47:05 -0400

    Create ProductsContext and ProductsProvider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './context/user.context';
+import { ProductsProvider } from './context/products.context';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,12 +11,10 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../context/products.context";
+
+const Shop = () => {
+	const { products } = useContext(ProductsContext);
+	return (
+		<div>
+			{products.map(({ id, name }) => (
+				<div key={id}>
+					<h1>{name}</h1>
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default Shop;
